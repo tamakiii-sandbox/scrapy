4fd49d430854e5a433bff96c0542db56f5d5c58a
tamakiii <tamakiii@users.noreply.github.com>
2019-05-04 12:10:15 +0900

    connpass spider

--- src/spiders/connpass.py
+++ src/spiders/connpass.py
@@ -1,11 +1,32 @@
 # -*- coding: utf-8 -*-
+import re
+import warnings
+from urllib.parse import urlparse
+
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.utils.httpobj import urlparse_cached
 
-
-class ConnpassSpider(scrapy.Spider):
+class ConnpassSpider(CrawlSpider):
     name = 'connpass'
-    allowed_domains = ['conpass.com']
-    start_urls = ['http://conpass.com/']
+    allowed_domains = ['connpass.com']
+    start_urls = [
+        'https://connpass.com/explore/'
+    ]
 
     def parse(self, response):
-        pass
+        for link in LinkExtractor(canonicalize=True, unique=True, allow='/event/\d+/.*').extract_links(response):
+            if self.is_allowed(link.url):
+                yield {
+                    'from': response.url,
+                    'to': link.url,
+                }
+
+        for link in LinkExtractor(canonicalize=True, unique=True, deny='/event/\d+/.*').extract_links(response):
+            if self.is_allowed(link.url):
+                yield response.follow(response.urljoin(link.url), callback=self.parse)
+
+    def is_allowed(self, url):
+        regex = re.compile(r'^(.*\.)?(%s)$' % '|'.join(self.allowed_domains))
+        return bool(regex.search(urlparse(url).netloc))
